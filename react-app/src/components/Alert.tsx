f1c3890b2d7f2c2292a31ecfeec13fa20e18d6ee
harolinenoesi <harolinenoesi@gmail.com>
2024-10-14 23:12:08 -0400

    finished tutorial

--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -1,3 +1,20 @@
-export const Alert = () => {
-  return <div>Alert</div>;
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  onClose: () => void;
+}
+
+export const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className="alert alert-primary alert-didsmissble">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+      />
+    </div>
+  );
 };
